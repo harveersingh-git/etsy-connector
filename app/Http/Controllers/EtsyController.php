84b670e8bfc7443e991a37ef2bba9a4038be8213
Harveer Singh <harveersingh@virtualemployee.com>
2022-03-03 15:57:37 +0530

    shop name check

--- app/Http/Controllers/EtsyController.php
+++ app/Http/Controllers/EtsyController.php
@@ -231,7 +231,7 @@ class EtsyController extends Controller
                 $curl = curl_init();
 
                 curl_setopt_array($curl, array(
-                    CURLOPT_URL => 'https://openapi.etsy.com/v2/shops/:' . $shop_id . '/listings/active?api_key=' . $key_string,
+                    CURLOPT_URL => 'https://openapi.etsy.com/v2/shops/' . $shop_id . '/listings/active?api_key=' . $key_string,
                     CURLOPT_RETURNTRANSFER => true,
                     CURLOPT_ENCODING => '',
                     CURLOPT_MAXREDIRS => 10,
