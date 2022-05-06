064f6816d6af9cad14282880f332471161728a80
Harveer Singh <harveersingh@virtualemployee.com>
2022-05-06 19:09:30 +0530

    remove static pages

--- resources/views/admin/layout/leftsidebar.blade.php
+++ resources/views/admin/layout/leftsidebar.blade.php
@@ -18,8 +18,8 @@
                 <span>Welcome,</span>
                 <a href="javascript:void(0);" class="dropdown-toggle user-name" data-toggle="dropdown"><strong> {{ Auth::user()->name }}</strong></a>
                 <ul class="dropdown-menu dropdown-menu-right account">
-                    <li><a href="page-profile.html"><i class="icon-user"></i>My Profile</a></li>
-                    <li><a href="app-inbox.html"><i class="icon-envelope-open"></i>Messages</a></li>
+                    <li><a href="javascript:void(0);"><i class="icon-user"></i>My Profile</a></li>
+                    <li><a href="javascript:void(0);"><i class="icon-envelope-open"></i>Messages</a></li>
                     <li><a href="javascript:void(0);"><i class="icon-settings"></i>Settings</a></li>
                     <li class="divider"></li>
                     <li><a href="page-login.html"><i class="icon-power"></i>Logout</a></li>
