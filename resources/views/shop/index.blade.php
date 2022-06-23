a332cb6ee0dc6ab035eb20b3132bdd77248c574d
Harveer Singh <harveersingh@virtualemployee.com>
2022-06-23 20:18:58 +0530

    remove app url column in the list

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -56,7 +56,7 @@
                                         <th class="text-center">{{__('messages.sr_no')}}</th>
                                         <th class="text-center">{{__('messages.shop_name')}}</th>
                                       
-                                        <th>{{__('messages.action')}}</th>
+                                        <th class="text-center">>{{__('messages.action')}}</th>
                                 </thead>
                                 <tfoot>
                                     <tr>
@@ -74,7 +74,7 @@
                                         <td class="text-center">{{$value->shop_name}}</td>
                                      
 
-                                        <td>
+                                        <td class="text-center">>
                                             <a type="button" href="{{url('/shoplist/edit')}}/{{base64_encode($value->id)}}" class="btn btn-info" title="Edit" style="color: #fff;"><i class="fa fa-edit"></i></a>
                                             <!-- <a type="button" href="{{url('/etsy-config')}}/{{$value->id}}" class="btn btn-warning" title="Generate Token And Authorize" style="color: #fff;"><i class="fa fa-gear fa-spin"></i></a> -->
 
