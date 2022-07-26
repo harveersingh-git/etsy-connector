898f38572b9817ef8023f5f44585c20b7f260e61
Harveer Singh <harveersingh@virtualemployee.com>
2022-07-26 21:13:58 +0530

    logo issue in email

--- resources/views/emails/account_create.blade.php
+++ resources/views/emails/account_create.blade.php
@@ -16,7 +16,7 @@
                         <tr>
                             <td align="center" style="padding:20px 0 20px 0;background:#5ba836;">
                                 <a href="#" style="font-size: 24px; color: #fff; list-style: none; text-decoration: none;">
-                                    <img src="https://connector.besirious.dev/public/assets/images/icon-light.svg" alt="" width="50" style="height: auto; display: inline-block; vertical-align: middle;" /> Etsy Connector
+                                    <img src="{{asset('assets/images/icon-light.svg')}}" alt="" width="50" style="height: auto; display: inline-block; vertical-align: middle;" /> Etsy Connector
                                 </a>
                             </td>
                         </tr>
