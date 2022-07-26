df48b210ecf860f1a2be2fab5674d860d61d1696
Harveer Singh <harveersingh@virtualemployee.com>
2022-07-26 20:23:26 +0530

    change email template

--- resources/views/emails/verify.blade.php
+++ resources/views/emails/verify.blade.php
@@ -1,66 +1,53 @@
 <!DOCTYPE html>
 <html>
-
 <head>
     <meta charset="utf-8">
     <title>Email Template</title>
 </head>
-
 <body>
-
-    <body style="margin:0;padding:0;">
-        <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;background:#ffffff;">
-            <tr>
-                <td align="center" style="padding:0;">
-                    <table role="presentation" style="width:800px;border-collapse:collapse;border:1px solid #cccccc;border-spacing:0;text-align:left; background-color: #3c3c3b;">
-                        <tr>
-                            <td align="center" style="padding:20px 0 20px 0;background:#5ba836;">
-                                <a href="#" style="font-size: 24px; color: #fff; list-style: none; text-decoration: none;">
-                                    <img src="https://connector.besirious.dev/public/assets/images/icon-light.svg" alt="" width="50" style="height: auto; display: inline-block; vertical-align: middle;" /> Etsy Connector
-                                </a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td style="padding:36px 30px 42px 30px;">
-                                <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;">
-                                    <tr>
-                                        <td style="padding:0 0 20px 0;color:#fff;">
-                                            <h1 style="font-size:24px;margin:0 0 20px 0;font-family:Arial,sans-serif; color: #fff;">Hello!</h1>
-                                            <p style="margin:0 0 12px 0;font-size:14px;line-height:24px;font-family:Arial,sans-serif; color: #fff;">We just need to verify your email address before you can access connector.besirious</p>
-                                        </td>
-                                    </tr>
-                                    <tr style="text-align: center;">
-                                        <td style="padding:0;">
-                                            <a href="{{ $url }}" style="padding: 13px 28px; color: #fff; background-color: #5ba836; border: none;   font-size: 15px; cursor: pointer;">Verify Email Address</a>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td style="padding:20px 0 0px 0;color:#fff;">
-                                            <p style="margin:0 0 12px 0;font-size:14px;line-height:24px;font-family:Arial,sans-serif; color: #fff;">This link will verify your email address, and then you’ll officially be a part of the connector.besirious.</p>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td style="padding:20px 0 0px 0;color:#fff;">
-                                            <p style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif; color: #fff;">Regards,<br>etsy-connector</p>
-                                        </td>
-                                    </tr>
-                                </table>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td align="center" style="padding:20px 0 20px 0;background:#5ba836;">
-                                <a href="#" style="font-size: 18px; color: #fff; list-style: none; text-decoration: none;">
-                                    
-                                        © Copyright 2022 besirious.
-                                  
-                                </a>
-                            </td>
-                        </tr>
-                    </table>
-                </td>
-            </tr>
-        </table>
-    </body>
+<body style="margin:0;padding:0;">
+    <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;background:#ffffff;">
+        <tr>
+            <td align="center" style="padding:0;">
+                <table role="presentation" style="width: 600px; border-collapse: collapse; border-spacing: 0; text-align: left;    background-color: #fff; overflow: hidden; border-radius: 10px; box-shadow: 0px 3px 7px 1px #3c3c3b26;">
+                    <tr>
+                        <td align="center" style="padding:40px 0 40px 0;background:#5ba836;">
+                            <a href="#" style="font-size: 35px; color: #fff; list-style: none; font-family:Arial,sans-serif;text-decoration:none !important; display: inline-block;">
+                            <img src="http://65.2.143.196/anadev/etsy-connector/public/assets/images/icon-light.svg" alt="" width="80" style="height: auto; display: inline-block; vertical-align: middle;" /><span style="text-decoration:none !important;display: inline-block;">&nbsp;Etsy Connector</span>
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="padding:36px 30px 42px 30px; background-color:#fff; text-align: center; border:1px solid #ddd;">
+                            <table role="presentation" style="width:70%;border-collapse:collapse;border:0;border-spacing:0; margin: 0 auto;">
+                                <tr>
+                                    <td style="padding:0 0 20px 0;color:#3c3c3b;">
+                                        <h1 style="font-size:24px;margin:0 0 20px 0;font-family:Arial,sans-serif; color: #3c3c3b;">Hello!</h1>
+                                        <p style="margin:0 0 12px 0;font-size:17px;line-height:24px;font-family:Arial,sans-serif; color: #3c3c3b;">Please click the button below to verify your email address</p>
+                                    </td>
+                                </tr>
+                                <tr style="text-align: center;">
+                                    <td style="padding:0 0 12px 0;">
+                                        <a href="{{ $url }}" style="padding: 13px 40px; color: #fff; background-color: #3c3c3b; border: none;   font-size: 16px; cursor: pointer; font-family:Arial,sans-serif; list-style: none;text-decoration:none !important; display: inline-block; "><span style="display: inline-block;">Verify Email Address</span></a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="padding:20px 0 0px 0;color:#3c3c3b;">
+                                        <p style="margin:0 0 12px 0;font-size:17px;line-height:24px;font-family:Arial,sans-serif; color: #3c3c3b;">If you did not create an account, no further action is required.</p>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="padding:20px 0 0px 0;color:#3c3c3b;">
+                                        <p style="margin:0 0 12px 0;font-size:17px;font-family:Arial,sans-serif; color: #3c3c3b;">Regards,<br>etsy-connector</p>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
 </body>
-
+</body>
 </html>
